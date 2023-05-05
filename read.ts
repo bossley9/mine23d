c15deafda8e02e4b4a961bb365cc5b1a045c587d
Sam Bossley <bossley.samuel@gmail.com>
2023-05-05 07:57:41 -0700

    feat: attempt to parse nbt data

--- read.ts
+++ read.ts
@@ -1,15 +1,7 @@
+import { parseNBTFile } from './nbt.ts'
+
 export async function readSaveFolder(saveFolder: string) {
   const levelDataFilePath = saveFolder.replace(/\/$/, '') + '/level.dat'
 
-  const reader = (await Deno.open(levelDataFilePath))
-    .readable
-    .pipeThrough(new DecompressionStream('gzip'))
-    .pipeThrough(new TextDecoderStream())
-    .getReader()
-
-  let state = await reader.read()
-  while (!state.done) {
-    console.log('CHUNK:', state.value)
-    state = await reader.read()
-  }
+  await parseNBTFile(levelDataFilePath)
 }
